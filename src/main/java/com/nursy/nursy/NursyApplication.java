95a5abfe15958ec06dce419dc40258e8080efab4
kwonjieon <holemart@naver.com>
2024-12-07 22:58:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/nursy/nursy/NursyApplication.java
@@ -0,0 +1,13 @@
+package com.nursy.nursy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NursyApplication.class, args);
+    }
+
+}
