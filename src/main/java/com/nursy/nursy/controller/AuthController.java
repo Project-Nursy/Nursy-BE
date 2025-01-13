bc43b25dd7218b96c44131759d72e5fe10099eea
kwonjieon <holemart@naver.com>
2025-01-13 12:55:23 +0900

    feat: 로그인 , 로그아웃 구현

--- src/main/java/com/nursy/nursy/controller/AuthController.java
+++ src/main/java/com/nursy/nursy/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.nursy.nursy.controller;
 
 import com.nursy.nursy.domain.dto.join.JoinRequest;
+import com.nursy.nursy.domain.dto.login.AccessTokenResponseDto;
 import com.nursy.nursy.domain.dto.login.LoginRequest;
 import com.nursy.nursy.domain.entity.User;
 import com.nursy.nursy.jwt.JwtToken;
 import com.nursy.nursy.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -32,8 +36,35 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         JwtToken jwtToken = authService.login(loginRequest);
+        AccessTokenResponseDto accessTokenResponseDto = new AccessTokenResponseDto();
+        accessTokenResponseDto.setAccessToken(jwtToken.getAccessToken());
 
-        return ResponseEntity.status(200).body(jwtToken);
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", jwtToken.getRefreshToken())
+                .httpOnly(true)
+                .secure(true) // HTTPS 환경에서만 사용
+                .path("/")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .body(accessTokenResponseDto);
+
+        //return ResponseEntity.status(200).body(jwtToken);
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        // RefreshToken 쿠키 삭제
+        ResponseCookie deleteCookie = ResponseCookie.from("refreshToken", "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .build();
+
+        // 응답에 삭제된 쿠키 설정
+        response.addHeader(HttpHeaders.SET_COOKIE, deleteCookie.toString());
+
+        return ResponseEntity.ok("로그아웃 되었습니다.");
     }
     @GetMapping("/test")
     public ResponseEntity<?> test(Authentication authentication) {
