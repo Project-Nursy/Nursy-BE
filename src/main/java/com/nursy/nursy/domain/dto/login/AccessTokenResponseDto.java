65300c5c769cb69e61db36a560c0dc1752e46da6
kwonjieon <holemart@naver.com>
2025-01-13 12:54:55 +0900

    feat: jwt토큰 응답 수정

---
+++ src/main/java/com/nursy/nursy/domain/dto/login/AccessTokenResponseDto.java
@@ -0,0 +1,10 @@
+package com.nursy.nursy.domain.dto.login;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AccessTokenResponseDto {
+    String accessToken;
+}
--- src/main/java/com/nursy/nursy/jwt/JwtTokenUtil.java
+++ src/main/java/com/nursy/nursy/jwt/JwtTokenUtil.java
@@ -1,8 +1,11 @@
 package com.nursy.nursy.jwt;
 
+import com.nursy.nursy.domain.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,10 +21,11 @@ public class JwtTokenUtil {
     @Value("${jwt.expire.refresh}")
     private Long refreshTokenExpireMs;
     //토큰발급
-    public String createToken(String userId,String type ,Long expireTimeMs){
+    public String createToken(String userIdentifier,String role,String type ,Long expireTimeMs){
         Claims claims = Jwts.claims();
-        claims.put("userId", userId);
-
+        //claims.put("userId", user.getCno().toString());
+        claims.put("userId", userIdentifier);
+        claims.put("role",role);
         return Jwts.builder()
                 .setHeaderParam("typ", type)
                 .setClaims(claims)
@@ -30,16 +34,20 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS256,secret)
                 .compact();
     }
-    public String createAccessToken(String userId){
-        return createToken(userId,"access",accessTokenExpireMs);
+    public String createAccessToken(String userIdentifier,String role){
+        return createToken(userIdentifier,role,"access",accessTokenExpireMs);
     }
-    public String createRefreshToken(String userId){
-        return createToken(userId,"refresh",refreshTokenExpireMs);
+    public String createRefreshToken(String userIdentifier,String role){
+        return createToken(userIdentifier,role,"refresh",refreshTokenExpireMs);
     }
 
     public String getUserId(String token){
         return extractClaims(token).get("userId",String.class);
     }
+    public String getUserRole(String token){
+        return extractClaims(token).get("role",String.class);
+    }
+
     // 토큰 만료 확인
     public boolean isExpired(String token) {
         try {
@@ -61,5 +69,16 @@ public class JwtTokenUtil {
         } catch (Exception e) {
             throw new IllegalArgumentException("Invalid token", e);
         }
+    }
+    public String getRefreshTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refreshToken")) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
