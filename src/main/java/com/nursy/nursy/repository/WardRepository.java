99e205dba5be8864c8b2735993d3e21031f13781
kwonjieon <holemart@naver.com>
2025-01-27 17:30:59 +0900

    feat: ward 생성시 wardSetting데이터도 같이 생성

--- src/main/java/com/nursy/nursy/repository/WardRepository.java
+++ src/main/java/com/nursy/nursy/repository/WardRepository.java
@@ -2,12 +2,18 @@ package com.nursy.nursy.repository;
 
 
 import com.nursy.nursy.domain.entity.Ward;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WardRepository extends JpaRepository<Ward,Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Ward save(Ward ward);
+
     Optional<List<Ward>> findByUserId(String userId);
 
     void deleteByUserIdAndWardId(String userId, Long wardId);
