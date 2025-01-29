8a53e7d0171ed2d42c0fbec51be13a6df6b047d5
kwonjieon <holemart@naver.com>
2025-01-29 17:07:11 +0900

    feat: wardSetting 업데이트, 병동삭제시 같이 삭제 기능 구현

---
+++ src/main/java/com/nursy/nursy/repository/WardSettingRepository.java
@@ -0,0 +1,8 @@
+package com.nursy.nursy.repository;
+
+import com.nursy.nursy.domain.entity.WardSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WardSettingRepository extends JpaRepository<WardSetting,Long> {
+
+}
